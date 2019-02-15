c2bde3f9a0cf7c75ca08c8acaa062815c44add85
Eduardo <jecorona97@gmail.com>
2019-02-14 18:57:00 -0800

    read files
    added config file

--- indexer.py
+++ indexer.py
@@ -2,28 +2,34 @@
 
 import database
 import os
+import config
 
 FILES = []
 
 
 def read_directory(directory):
-    for item in directory:
-        if os.path.isdir(os.path.join('.', item)):
-            read_directory(item)
+    for item in os.listdir(directory):
+        subitem = os.path.join(directory, item)
+        if os.path.isdir(subitem):
+            read_directory(subitem)
         else:
-            print(item)
+            with open(subitem, 'r') as f:
+                print(subitem)
 
 
 def main():
-    sub_directories = os.listdir('WEBPAGES_RAW')
+
+
     # Assuming the main directory contains directories
     # and each directory contains files
-    for item in sub_directories:
-        if os.path.isdir(os.path.join(item)):
-            print(item, 'isDir')
-            read_directory(item)
+    for item in os.listdir(config.RAW_WEBPAGES):
+        subdir = os.path.join(config.RAW_WEBPAGES, item)
+        if os.path.isdir(subdir):
+            read_directory(subdir)
+            break
         else:
-            print(item, 'not a directory')
+            print("*"*10,item)
+
 
 
 if __name__ == '__main__':
