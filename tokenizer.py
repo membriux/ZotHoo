531ea99124b4a05a60aa3047aa82eea35097c512
Eduardo <jecorona97@gmail.com>
2019-02-14 20:27:01 -0800

    tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,24 @@
+import re
+from collections import Counter
+from nltk.corpus import stopwords 
+from nltk.stem.porter import PorterStemmer
+
+class Tokenizer():
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english')) 
+        self.stemmer = PorterStemmer()
+
+    @staticmethod
+    def normalize_text(corpus : str):
+        temp = re.sub(r"([^ \sa-zA-Z0-9])", ' ', corpus).lower()  # srip non ascii
+        temp = re.sub(r"( +)", ' ', temp).strip()     # remove extra spaces
+        return temp
+
+
+    def counter_tokenize(self, corpus : str, stem=False):
+        temp = self.normalize_text(corpus).split(' ')
+        if self.stemmer: temp = [self.stemmer.stem(word) for word in temp]
+        return Counter((word for word in temp if word not in self.stop_words) )
+
+
+        
