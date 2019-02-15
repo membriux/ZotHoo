7c35961584e025c8d1c4e6feedad9262c3aac34b
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-02-14 18:34:35 -0800

    Added basic os.path functionality

--- indexer.py
+++ indexer.py
@@ -1,2 +1,37 @@
 
-import 
+
+import database
+import os
+
+FILES = []
+
+
+def read_directory(directory):
+    for item in directory:
+        if os.path.isdir(os.path.join('.', item)):
+            read_directory(item)
+        else:
+            print(item)
+
+
+def main():
+    sub_directories = os.listdir('WEBPAGES_RAW')
+    # Assuming the main directory contains directories
+    # and each directory contains files
+    for item in sub_directories:
+        if os.path.isdir(os.path.join(item)):
+            print(item, 'isDir')
+            read_directory(item)
+        else:
+            print(item, 'not a directory')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
