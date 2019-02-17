67fcbe298b27c761981363653468030660c16d05
Eduardo <jecorona97@gmail.com>
2019-02-16 18:49:12 -0800

    merge from ed

--- indexer.py
+++ indexer.py
@@ -8,27 +8,24 @@ FILES = []
 
 
 def read_directory(directory):
+    _files = []
     for item in os.listdir(directory):
         subitem = os.path.join(directory, item)
         if os.path.isdir(subitem):
-            read_directory(subitem)
+            _files = _files + read_directory(subitem)
         else:
-            with open(subitem, 'r') as f:
-                print(subitem)
+            _files.append(subitem)
+    return _files
 
 
 def main():
-
-
     # Assuming the main directory contains directories
     # and each directory contains files
     for item in os.listdir(config.RAW_WEBPAGES):
         subdir = os.path.join(config.RAW_WEBPAGES, item)
         if os.path.isdir(subdir):
             read_directory(subdir)
-            break
-        else:
-            print("*"*10,item)
+
 
 
 if __name__ == '__main__':
--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup as bs
     Methods: counter, tokenizer
 
 """
-
+invalid_files = []
 
 invalid_tags = ['style', 'script', 'a', 'meta',
                 'link', 'ul', 'li', 'table', 'td',
@@ -18,19 +18,22 @@ invalid_tags = ['style', 'script', 'a', 'meta',
 html_parser = 'html.parser'
 
 
-def parse(file) -> [str]:
+def parse(_file) -> str:
     all_text = ''
     # Open file and read it using bs4
-    with open(file) as f:
-        soup = bs(f, html_parser)
+    try:
+        with open(_file, 'r', encoding="utf-8") as f:
+            soup = bs(f, html_parser)
+            # Remove unnecessary text from file
+            for i in invalid_tags:
+                while soup.find(i) is not None:
+                    soup.find(i).decompose()
 
-        # Remove unnecessary text from file
-        for i in invalid_tags:
-            while soup.find(i) is not None:
-                soup.find(i).decompose()
-
-        all_text += soup.get_text()
-        return all_text
+            all_text += soup.get_text()
+    except Exception as _:
+        invalid_files.append(_file)
+    
+    return all_text
 
 
 if __name__ == '__main__':
