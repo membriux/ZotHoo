56e8c43463eb705a603f20ec9e6b0fa689a651d3
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-02-16 17:23:36 -0800

    Drop database every time running app

--- database.py
+++ database.py
@@ -9,16 +9,15 @@ db = client['ICSdatabase']
 
 
 def test_db_connection():
-    print('DB: testing connection')
-    print('DB: adding URL col')
     print('DB: Databases:', client.list_database_names())
-    print('DB: Successfully created col: URLs')
+    print('DB: testing connection')
     url_cols = db['URLs']
     for col_item in url_cols.find():
         print(col_item)
 
 
 if __name__ == '__main__':
+    client.drop_database(db)
     test_db_connection()
 
 
