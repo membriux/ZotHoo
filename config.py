35fc0f3de81e3b17916e54046a4b079fd77296ee
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-02-20 21:58:56 -0800

    Finished webapp

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 RAW_WEBPAGES = "subsample"
+BOOKKEEPING = "WEBPAGES_RAW/bookkeeping.json"
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, url_for, render_template, request
+import json
+import config
+import pprint
+
+pp = pprint.PrettyPrinter()
 
 app = Flask(__name__)
 
-
-lst = ['Hello avocados', 'Hello world', 'Hello memo', 'Hello martin', 'Hello water', 'Hello Irvine']
+index = dict()
+bookkeeping = dict()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global index, bookkeeping
+    load_index()
     if request.method == 'POST' and request.form['search_input'] != '':
         search_input = request.form['search_input']
         results = run_search(search_input)
@@ -17,8 +24,16 @@ def index():
 
 
 def run_search(search_input):
-    results = []
-    for item in lst:
-        if search_input in item:
-            results.append(item)
-    return results
+    return [bookkeeping[u] for u in sorted(index[search_input], key=index[search_input].__getitem__, reverse=True)]
+
+
+def load_index():
+    global index, bookkeeping
+    with open('index.json', 'r') as data:
+         index = json.load(data)
+
+    with open(config.BOOKKEEPING, 'r') as data:
+         bookkeeping = json.load(data)
+
+
+
