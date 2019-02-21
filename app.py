ba4e119a153e8572f4887b6a8c0f4138a2c71375
Eduardo <jecorona97@gmail.com>
2019-02-20 16:24:56 -0800

    added build_index

--- app.py
+++ app.py
@@ -3,27 +3,50 @@ import os
 import pymongo
 from tqdm import tqdm
 
+from collections import OrderedDict
 from tokenizer import Tokenizer
 from indexer import read_directory
 from parser import parse
 import config
 
-def main():
-    client = pymongo.MongoClient('mongodb://localhost:27017/')
-    client.drop_database("ICSdatabase")
-    db = client['ICSdatabase']
-    tokenizer = Tokenizer()
+Index = OrderedDict()
 
+def build_index():
+    global Index
+    tokenizer = Tokenizer()
     for subdir in tqdm(os.listdir(config.RAW_WEBPAGES)):
+        # print("subdir", subdir)
         full_subdir = os.path.join(config.RAW_WEBPAGES, subdir)
         if os.path.isdir(full_subdir):
             to_parse = read_directory(full_subdir)
 
             for _file in to_parse:
+                filename = "/".join(_file.split("/")[1:])
+                # print("filename", filename)
                 parsed_txt = parse(_file)
                 token_counter = tokenizer.counter_tokenize(parsed_txt)
                 for tok in token_counter:
-                    print(tok)
+                    if tok not in Index:
+                        Index[tok] = { filename : token_counter[tok]}
+                    else:
+                        Index[tok][filename] = token_counter[tok]
+
+        
+
+
+
+def main():
+    # client = pymongo.MongoClient('mongodb://localhost:27017/')
+    # client.drop_database("ICSdatabase")
+    # db = client['ICSdatabase']
+    build_index()
+    for i, (key, value) in enumerate(Index.items()):
+        print("[{}] {}\t{}".format(i, key, value))
+        if i == 100:
+            break
+    
+
+    
 
 
 
