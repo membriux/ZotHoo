1d21f4b0ab844119a2e0c106f325c68e9e8c8cce
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-02-21 19:13:43 -0800

    updated to have necessary items

--- app.py
+++ app.py
@@ -11,16 +11,18 @@ app = Flask(__name__)
 
 index = dict()
 bookkeeping = dict()
+
 total_tokens = 0
 total_links = 0
 N_documents = None
+N_tokens = None
 tokenizer = Tokenizer()
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global index, bookkeeping, total_tokens, total_links
-    load_index()
     if request.method == 'POST' and request.form['search_input'] != '':
         search_input = request.form['search_input']
         results = run_search(search_input)
@@ -28,12 +30,16 @@ def index():
             search_input = 'No results for', search_input
         return render_template('index.html',
                                results=results, search=search_input,
-                               links=total_links, tokens=total_tokens)
+                               links=total_links, tokens=total_tokens,
+                               all_tokens=N_tokens, all_documents=N_documents)
     else:
-        return render_template('index.html')
+        return render_template('index.html',
+                               all_tokens=N_tokens, all_documents=N_documents)
+
 
 def tfidf(x, N) -> float:
     return x * log10(N_documents / N)
+
 
 def run_search(search_input):
     global total_tokens, total_links
@@ -43,23 +49,26 @@ def run_search(search_input):
         total_links = len(index[processed_query])
         total_tokens = sum([count for count in index[processed_query].values()])
         tfidf_scores = [(k, tfidf(v, len(index[processed_query]) ) ) for k, v in index[processed_query].items()]
-        return [bookkeeping[u] for u,v in sorted(tfidf_scores, key=lambda x : x[1], reverse=True)][:config.TOP_N_results]
+        return [bookkeeping[u] for u, v in sorted(tfidf_scores, key=lambda x: x[1], reverse=True)][:config.TOP_N_results]
     except KeyError:
         print("[ERROR] Empty query. Raw: \"{}\", Processed: \"{}\"]".format(search_input, processed_query))
         return []
 
 
 def load_index():
-    global index, bookkeeping, N_documents
+    global index, bookkeeping, N_documents, N_tokens
     with open(config.INDEX_PATH, 'r') as data:
-         index = json.load(data)
+        index = json.load(data)
 
     with open(config.BOOKKEEPING, 'r') as data:
-         bookkeeping = json.load(data)
+        bookkeeping = json.load(data)
 
     N_documents = len(bookkeeping)
+    N_tokens = len(index)
 
 
 if __name__ == '__main__':
+    load_index()
     app.run()
+
 
