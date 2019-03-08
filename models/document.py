d851e93d412b9c300231b290a52dc367966a05f5
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-03-07 21:53:52 -0800

    parsing fixed with links like facebook

--- models/document.py
+++ models/document.py
@@ -16,13 +16,14 @@ class Document:
         soup = BeautifulSoup(c, 'lxml')
         if soup.find('h1') != None:
             return soup.find('h1').get_text()
+        elif 'Facebook' in soup.title.get_text():
+            return soup.title.get_text()
         else:
-            return soup.findChildren()[0].get_text()[0:48] + '...'
+            return soup.findChildren('body')[0].get_text()[0:48] + '...'
 
 
 
-
-#
-# link = 'http://www.ics.uci.edu/~pattis/ICS-33/fact.html'
-# d = Document(link)
-# print('URL:\n\n', d.url,'HEADING\n\n', d.heading)
+if __name__ == '__main__':
+    # link = 'www.ics.uci.edu/facebook/login/browse/fanned_pages/search/str/%25E6%2595%2599%25E8%2582%25B2/keywords_pages/UCIBrenICS/photos/a.10150104180706909.283411.106291486908/10154351203306909?type=3'
+    # d = Document(link)
+    # print('URL:\n\n', d.url,'HEADING\n\n', d.heading)
--- parser.py
+++ parser.py
@@ -18,12 +18,17 @@ html_parser = 'html.parser'
 
 def parse(_file) -> str:
     all_text = ''
+    header = ''
     # Open file and read it using bs4
     try:
         with open(_file, 'r', encoding="utf-8") as f:
             soup = bs(f, html_parser)
-            # Remove unnecessary text from file
+            # Get header
+
+            header = get_header(soup)
+
             for tag in valid_tags:
+
                 soup.find_all(tag)
 
             all_text += soup.get_text()
@@ -31,15 +36,27 @@ def parse(_file) -> str:
     except Exception as _:
         print('invalid: ', _file)
         invalid_files.append(_file)
-    
+
     return all_text
+
+def get_header(soup):
+        result = requests.get('http://' + self.url)
+        c = result.content
+        soup = BeautifulSoup(c, 'lxml')
+        if soup.find('h1') != None:
+            return soup.find('h1').get_text()
+        elif 'Facebook' in soup.title.get_text():
+            return soup.title.get_text()
+        else:
+            return soup.findChildren('body')[0].get_text()[0:48] + '...'
+
 
 
 def remove_invalids(string):
     return ''.join([c if ord(c) < 128 else ' ' for c in string])
 
 
+
 if __name__ == '__main__':
     file = config.RAW_WEBPAGES + '/2/10'
     print(parse(file))
-
