a5f232e4cdb118f934b02d034504a65b15bf2cf8
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-03-07 23:27:49 -0800

    Update document.py

--- models/document.py
+++ models/document.py
@@ -13,9 +13,16 @@ class Document:
         result = requests.get('http://' + self.url)
         c = result.content
         soup = BeautifulSoup(c, 'lxml')
+        header = ''
         if soup.find('h1') != None:
             return soup.find('h1').get_text()
         elif soup.find('title') != None and 'Facebook' in soup.find('title').get_text():
             return soup.find('title').get_text()
+        elif soup.find('body') != None:
+            s = soup.findChildren('body')[0].get_text()
+            header = re.sub('\n|\t', '', s)
+            return header[0:48]
         else:
-            return soup.findChildren('body')[0].get_text()[0:48] + '...'
+            s = soup.get_text()
+            header = re.sub('\n|\t', '', s)
+            return header[0:48]
